4ca9c74a5dde4c0edf7f7e50e74721208641363d
Gass <madebygass@gmail.com>
2021-09-03 13:02:46 +0200

    [feat] Added prisma

--- web/todo_app/src/modules/note/noteRouter.js
+++ web/todo_app/src/modules/note/noteRouter.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 class Notes extends React.Component {
-    //State of application component
-    state = {
-        notes: [],
-        error: null,
-    };
+  //State of application component
+  state = {
+    notes: [],
+    error: null,
+  };
 
-    // Fetch of notes immediately after the component is mounted
-    componentDidMount = async () => {
-        try {
-            const response =await axios.get('https://strapi.naoroy.dev/');
-            this.setState({ notes: response.data });
-        } catch (error) {
-            this.setState({ error });
-        }
-    };
-    render() {
-        const {error} = this.state;
-
-        // Print errors if analytics
-        if (error) {
-            return <div> An error occured: {error.message}</div>;
-        }
-
-        return (
-            <div className="Notes">
-                <ul>
-                    {this.states.notes.map(note => (
-                            <li key={note.id}>{note.name}</li>
-                    ))}
-                </ul>
-            </div>
-        );
+  // Fetch of notes immediately after the component is mounted
+  componentDidMount = async () => {
+    try {
+      const response = await axios.get("https://strapi.naoroy.dev/");
+      this.setState({ notes: response.data });
+    } catch (error) {
+      this.setState({ error });
     }
+  };
+  render() {
+    const { error } = this.state;
+
+    // Print errors if analytics
+    if (error) {
+      return <div> An error occured: {error.message}</div>;
+    }
+
+    return (
+      <div className="Notes">
+        <ul>
+          {this.states.notes.map((note) => (
+            <li key={note.id}>{note.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Notes;
